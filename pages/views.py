f34158238426b320f3251dde8677e75feed7a282
m13D77i <m13d77i@gmail.com>
2025-01-17 20:22:22 +0330

    connected template to views ---> home/about me/contact me

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 
 def home_page_view(request):
-    return render(request,)
+    return render(request, "pages/home.html")
 
 def about_page_view(request):
-    return render(request, )
+    return render(request, "pages/about.html")
 
 def contact_page_view(request):
-    return render(request, )
+    return render(request, "pages/contact.html")
 
