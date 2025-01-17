78e1063360ad5ae7f6483b9b2d714bab08338c8b
m13D77i <m13d77i@gmail.com>
2025-01-17 17:50:01 +0330

    created home.html,about.html and contact.html

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 
 def home_page_view(request):
-    pass
+    return render(request,)
 
 def about_page_view(request):
-    pass
+    return render(request, )
 
 def contact_page_view(request):
-    pass
+    return render(request, )
 
