14fcdd5cdabd61ffc43a654b78c32d32c347cd72
m13D77i <m13d77i@gmail.com>
2025-01-18 17:47:12 +0330

    config db ----> postgresql

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASS"),
+        "HOST": "localhost",
+        "PORT": env.int("DB_PORT"),
     }
 }
 
