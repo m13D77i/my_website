f474014717879d6d12ed33177b25ee6b6ff708e4
m13D77i <m13d77i@gmail.com>
2025-01-18 18:48:28 +0330

    edit datatime filed in contact model

--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     fullname = models.CharField(blank=True,max_length=40, verbose_name="Fullname")
     email = models.EmailField(max_length=254, verbose_name="Email")
     description = models.TextField(blank=True,verbose_name="Description")
-    datatime_post = models.DateTimeField(auto_now_add=True, verbose_name="Datatime")
+    datatime_send = models.DateTimeField(auto_now_add=True, verbose_name="Datatime")
 
     def __str__(self):
         return self.fullname
